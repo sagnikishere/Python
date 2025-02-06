730ad13a1d3fa534849003b54f50b4414f2104ab
Sagnik Adhikary <166514625+sagnikishere@users.noreply.github.com>
2025-02-06 08:37:42 +0530

    Add files via upload

---
+++ add_array.py
@@ -0,0 +1,8 @@
+#input
+elements = int(input("enter nember of elements"))
+user_input = list(map(int, input("Enter numbers separated by space: ").split()))
+#Loop
+store = 0 
+for i in range(0,elements):
+    store = user_input[i] + store
+print(store)
